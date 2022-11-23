842f0a0148e5df84f87cb0c6d731255da2da84e8
Yinon bar <yinonbar1988@gmail.com>
2022-11-23 13:25:42 +0200

    fetch

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,8 +5,10 @@ function Home() {
   const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
     </div>
   );
 }
