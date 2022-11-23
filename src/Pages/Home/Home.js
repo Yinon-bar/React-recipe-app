1203717a62b14f8d5f0b6d94c63278be0b29ccf6
Yinon bar <yinonbar1988@gmail.com>
2022-11-23 13:18:55 +0200

    fetch

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,9 @@
+import { useFetch } from "../../Hooks/useFetch";
 import "./Home.css";
 
 function Home() {
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
   return (
     <div>
       <h1>Home</h1>
