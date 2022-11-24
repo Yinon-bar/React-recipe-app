f541f8015e9c1bf9eca61dc77563ee98e31e053b
Yinon bar <yinonbar1988@gmail.com>
2022-11-24 14:38:20 +0200

    individuale recipe

--- src/Pages/Create/Create.js
+++ src/Pages/Create/Create.js
@@ -1,7 +1,67 @@
+import { useState } from "react";
 import "./Create.css";
 
 function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        {/* ingredients goes here */}
+
+        <label>
+          <span>Recipe ingredients: </span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+            />
+            <button className="btn">add</button>
+          </div>
+        </label>
+
+        <label>
+          <span>Recipe method: </span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time (minutes): </span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          ></input>
+        </label>
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Create;
