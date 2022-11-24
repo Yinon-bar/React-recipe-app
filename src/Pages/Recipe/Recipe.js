43d6e440533605d121089c673c7579aabbc38a53
Yinon bar <yinonbar1988@gmail.com>
2022-11-24 13:29:13 +0200

    individule recipe

--- src/Pages/Recipe/Recipe.js
+++ src/Pages/Recipe/Recipe.js
@@ -15,7 +15,13 @@ function Recipe() {
       {recipe && (
         <>
           <h2 className="page-title">{recipe.title}</h2>
-          <p>Takes {recipe.cookingTime}</p>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
         </>
       )}
     </div>
